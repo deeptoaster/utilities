06a3409a1b9c8542808bcd3b755e794f854511ae
Deep Toaster <deeptoaster@gmail.com>
2020-02-23 13:43:41 -0800

    updated Orage calendar URL

--- orage.sh
+++ orage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -O /home/woot/Documents/orage/default.ics https://calendar.google.com/calendar/ical/yizhenwilliam%40gmail.com/private-3f8e0a2b7a28a65959bfcf9f2c166d0e/basic.ics
+wget -O /home/woot/Documents/orage/default.ics https://calendar.google.com/calendar/ical/deeptoaster%40gmail.com/private-5fa822723fe8706c3f0b2079e5ffe25f/basic.ics
 wget -O /home/woot/Documents/orage/hdbs.ics https://calendar.google.com/calendar/ical/n5h40cu30gv9u1jpcdcunpjm88%40group.calendar.google.com/private-2657fe6183a8a39a95816a3161d64452/basic.ics
 wget -O /home/woot/Documents/orage/house.ics https://calendar.google.com/calendar/ical/3084urjfo4f58br2m4n2njvh30%40group.calendar.google.com/private-a137d89e633665c414fdff6c3e22b2aa/basic.ics
 wget -O /home/woot/Documents/orage/job.ics https://calendar.google.com/calendar/ical/0hnod6aj3ncb7dbl24gtnq6qk0%40group.calendar.google.com/private-ac1d41fe31bd86731251c5e84aadb999/basic.ics
