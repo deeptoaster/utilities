91803fe34714f1e6d4df243681ee853c2dad1166
Deep Toaster <deeptoaster@gmail.com>
2020-04-30 04:59:32 -0400

    added pulseaudio routing

---
+++ loopback.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sink=$(pactl list sinks | grep alsa_output | sed -n 's/.*\bNom.: //p')
+pactl load-module module-null-sink sink_name=Input
+pactl load-module module-combine-sink sink_name=Output slaves=Input,$sink
+pactl load-module module-loopback source=$(pactl list sources | grep alsa_input | sed -n 's/.*\bNom.: //p') sink=Input
+
