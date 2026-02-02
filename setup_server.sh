624d8fd1cca8c18d3f6d7349ea8eb8b5c355ff68
Deep Toaster <deeptoaster@gmail.com>
2026-02-01 21:11:51 -0500

    switch to submodules for Vim packages

--- setup_server.sh
+++ setup_server.sh
@@ -13,7 +13,6 @@ sudo a2enmod vhost_alias
 sudo ln -fs $(pwd)/config/vhosts.conf /etc/apache2/conf-available/vhosts.conf
 sudo a2enconf vhosts
 sudo sed -Ei'' -e 's/^(display_errors|short_open_tag) = Off$/\1 = On/' /etc/php/*/*/php.ini
-rsync -Ir config/nvim ~/.config
 sudo update-locale LC_MESSAGES=fr_FR.UTF-8
 sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 50
 (echo 'paths:'; echo "  - $HOME/latexindent.yaml") > ~/indentconfig.yaml
